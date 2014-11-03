0ab6b994bd6069bb3e8b9505d375dd9440d16214
JakePickle <jmp6xb@mst.edu>
2014-11-03 10:12:15 -0600

    Implemented octokit.
    Used octokit to access github information on a specific user.

--- GitHubUpdateTest/Program.cs
+++ GitHubUpdateTest/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Octokit;
 
 namespace GitHubUpdateTest
 {
@@ -6,6 +8,13 @@ namespace GitHubUpdateTest
     {
         static void Main(string[] args)
         {
+            Task.Run(async () =>
+            {
+                var github = new GitHubClient(new ProductHeaderValue("MyAmazingApp"));
+                var user = await github.User.Get("half-ogre");
+                Console.WriteLine(user.Followers + " folks love the half ogre!");
+            }); 
+
             Console.Write("Console Test");
             Console.WriteLine();
             Console.ReadLine();
