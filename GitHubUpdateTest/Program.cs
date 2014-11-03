fe5c88027c3ef71c22c66337d72e33897cb99229
JakePickle <jmp6xb@mst.edu>
2014-11-03 09:43:35 -0600

    Writes to console
    added using System; and used Console.Write("Console Test");

--- GitHubUpdateTest/Program.cs
+++ GitHubUpdateTest/Program.cs
@@ -1,10 +1,12 @@
-﻿
+﻿using System;
+
 namespace GitHubUpdateTest
 {
     class Program
     {
         static void Main(string[] args)
-        {      
+        {
+            Console.Write("Console Test");
         }
     }
 }
