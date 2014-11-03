09d3560be96b620f2ec4cd84ed4e66b56873f168
JakePickle <jmp6xb@mst.edu>
2014-11-03 17:01:54 -0600

    Commented on code that uses octokit.

--- GitHubUpdateTest/Program.cs
+++ GitHubUpdateTest/Program.cs
@@ -10,14 +10,22 @@ namespace GitHubUpdateTest
         {
             Task.Run(async () =>
             {
-                var github = new GitHubClient(new ProductHeaderValue(""));
-                var user = await github.User.Get("tymorrow");
-                var repo = await github.Repository.GetBranch("tymorrow", "Launch-Pad", "master");
-                var releases = await github.Release.GetAll("JakePickle", "GitHubUpdateTest");
-                Console.WriteLine(user.Followers + " people follow Ty Morrow");
-                Console.WriteLine(repo.Commit.Sha);
-                Console.WriteLine(releases.Count);
-                Console.WriteLine(releases[0].TagName);
+                var github = new GitHubClient(new ProductHeaderValue(""));//Creates link to github
+
+                var user = await github.User.Get("tymorrow");//saves tymorrow's information to a user variable
+
+                var repo = await github.Repository.GetBranch("tymorrow", "Launch-Pad", "master");//saves repository information to repo variable
+
+                var releases = await github.Release.GetAll("JakePickle", "GitHubUpdateTest");//gets all releases of GitHubUpdateTest
+
+                Console.WriteLine(user.Followers + " people follow Ty Morrow");//prints number of people following tymorrow
+
+                Console.WriteLine(repo.Commit.Sha);//prints sha for current commit in Launc-Pad
+
+                Console.WriteLine(releases.Count);//prints number of releases for GitHubUpdateTest
+
+                Console.WriteLine(releases[0].TagName);//Prints the tag of the latest version of GitHubTestUpdate
+
             }); 
 
             Console.WriteLine("Console Test");
