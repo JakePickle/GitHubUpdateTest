ee838ee1568874ce32472d7cf194dbf7e4946f62
JakePickle <jmp6xb@mst.edu>
2014-11-03 20:09:45 -0600

    Downloads RED's latest release

--- GitHubUpdateTest/Program.cs
+++ GitHubUpdateTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Threading.Tasks;
 using Octokit;
 
@@ -16,7 +17,7 @@ namespace GitHubUpdateTest
 
                 var repo = await github.Repository.GetBranch("tymorrow", "Launch-Pad", "master");//saves repository information to repo variable
 
-                var releases = await github.Release.GetAll("JakePickle", "GitHubUpdateTest");//gets all releases of GitHubUpdateTest
+                var releases = await github.Release.GetAll("MST-MRDT", "Rover-Engagement-Display");//gets all releases of GitHubUpdateTest
 
                 Console.WriteLine(user.Followers + " people follow Ty Morrow");//prints number of people following tymorrow
 
@@ -25,6 +26,15 @@ namespace GitHubUpdateTest
                 Console.WriteLine(releases.Count);//prints number of releases for GitHubUpdateTest
 
                 Console.WriteLine(releases[0].TagName);//Prints the tag of the latest version of GitHubTestUpdate
+
+                Console.WriteLine("https://github.com/MST-MRDT/Rover-Engagement-Display/releases/download/" + releases[0].TagName + "/RED-" + releases[0].TagName + ".zip");
+
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile("https://github.com/MST-MRDT/Rover-Engagement-Display/releases/download/" + releases[0].TagName + "/RED-" + releases[0].TagName + ".zip", @"c:\users\ThePickle\downloads\RED-" + releases[0].TagName + ".zip");
+
+                Console.WriteLine("Done Downloading File");
+
+                //https://github.com/JakePickle/GitHubUpdateTest/releases/download/0.0.2/GitHubUpdateTest.exe
 
             }); 
 
