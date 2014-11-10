3925249fdd535022e965b7a4056dc4aacdece8c6
JakePickle <jmp6xb@mst.edu>
2014-11-10 16:56:58 -0600

    Updater now updates githubupdatetest

--- updater/Program.cs
+++ updater/Program.cs
@@ -19,24 +19,14 @@ namespace updater
             {
                 var github = new GitHubClient(new ProductHeaderValue("name"));//Creates link to github
 
-                var user = await github.User.Get("MST-MRDT");//saves MST-MRDT's information to a user variable
+                var releases = await github.Release.GetAll("JakePickle", "GitHubUpdateTest");//gets all releases of RED
 
-                var repo = await github.Repository.GetBranch("MST-MRDT", "Rover-Engagement-Display", "phoenix");//saves repository information to repo variable
+                Console.WriteLine(releases[0].TagName);//Prints the tag of the latest release
 
-                var releases = await github.Release.GetAll("MST-MRDT", "Rover-Engagement-Display");//gets all releases of RED
-
-                Console.WriteLine(user.Followers + " people follow MST-MRDT");//prints number of people following tymorrow
-
-                Console.WriteLine(repo.Commit.Sha);//prints sha for current commit in RED phoenix branch
-
-                Console.WriteLine(releases.Count);//prints number of releases for RED phoenix branch
-
-                Console.WriteLine(releases[0].TagName);//Prints the tag of the latest release of RED
-
-                Console.WriteLine("Downloading Latest Version(" + releases[0].TagName + ") of Rover Engagement Display");
+                Console.WriteLine("Downloading Latest Version(" + releases[0].TagName + ") of GitHubUpdateTest");
 
                 WebClient webClient = new WebClient();
-                webClient.DownloadFile("https://github.com/MST-MRDT/Rover-Engagement-Display/releases/download/" + releases[0].TagName + "/RED-" + releases[0].TagName + ".zip",
+                webClient.DownloadFile("https://github.com/JakePickle/GitHubUpdateTest/releases/download/" + releases[0].TagName + "GitHubUpdateTest.exe",// + releases[0].TagName + ".zip",
                     @dir.Substring(0, dir.Length - 20/*length of executables name including .exe*/) + releases[0].TagName + ".zip");
 
                 Console.WriteLine("Done Downloading File");
