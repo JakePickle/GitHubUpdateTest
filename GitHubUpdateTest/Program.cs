4c6fa80d3d0266d87cf86f208b73123aa34cd3ac
JakePickle <jmp6xb@mst.edu>
2014-11-10 16:53:01 -0600

    Now gets information on GitHubUpdateTest repo

--- GitHubUpdateTest/Program.cs
+++ GitHubUpdateTest/Program.cs
@@ -1,10 +1,25 @@
-﻿
+﻿using Octokit;
+using System;
+using System.Threading.Tasks;
+
 namespace GitHubUpdateTest
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            Task.Run(async () =>
+            {
+                var github = new GitHubClient(new ProductHeaderValue("name"));//Creates link to github
+
+                var releases = await github.Release.GetAll("JakePickle", "GitHubUpdateTest");//gets all releases
+
+                Console.WriteLine(releases[0].TagName);//Prints the tag of the latest release
+
+            });
+
+            Console.ReadLine();
             
         }
     }
