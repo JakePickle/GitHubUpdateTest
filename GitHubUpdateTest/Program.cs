3e2b1b792cb0a5ec6d50b1e61ab4416be7b30998
JakePickle <jmp6xb@mst.edu>
2014-11-18 20:37:24 -0600

    Now copies files to new directory

--- GitHubUpdateTest/Program.cs
+++ GitHubUpdateTest/Program.cs
@@ -10,6 +10,7 @@ namespace GitHubUpdateTest
         static void Main(string[] args)
         {
             string dir = System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase.Substring(8);
+            dir = dir.Substring(0, dir.Length - 20/*length of executables name including .exe*/);
 
             Task.Run(async () =>
             {
@@ -18,9 +19,36 @@ namespace GitHubUpdateTest
                 var releases = await github.Release.GetAll("JakePickle", "GitHubUpdateTest");//gets all releases
 
                 Console.WriteLine(releases[0].TagName);//Prints the tag of the latest release
+                
+                string fileName;
+                string sourcePath = dir;
+                Console.WriteLine(dir);
+                string targetPath = dir.Substring(0,dir.Length-6)+"Test/";
+                Console.WriteLine(targetPath);
+
+                // Use Path class to manipulate file and directory paths.
+                string sourceFile = System.IO.Path.Combine(sourcePath);
+                string destFile = System.IO.Path.Combine(targetPath);
+
+
+                if (System.IO.Directory.Exists(sourcePath))
+                {
+                    string[] files = System.IO.Directory.GetFiles(sourcePath);
+
+                    // Copy the files and overwrite destination files if they already exist.
+                    foreach (string s in files)
+                    {
+                        // Use static Path methods to extract only the file name from the path.
+                        fileName = System.IO.Path.GetFileName(s);
+                        destFile = System.IO.Path.Combine(targetPath, fileName);
+                        System.IO.File.Copy(s, destFile, true);
+
+                        Console.WriteLine("did file: " + fileName);
+                    }
+                }
 
                 Process updater = new Process();
-                updater.StartInfo.FileName = dir.Substring(0, dir.Length - 20/*length of executables name including .exe*/) + "/updater.exe";
+                updater.StartInfo.FileName = dir + "/updater.exe";
                 updater.Start();
 
                 System.Environment.Exit(1);
